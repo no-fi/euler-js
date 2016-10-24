8b3ef5198294596f525ca6cca5d8603bafafe0fe
Christopher Kiel <no-fi@users.noreply.github.com>
2016-10-24 17:44:05 -0500

    Cleans up Problem 2 test file

--- 2/test.spec.js
+++ 2/test.spec.js
@@ -3,21 +3,21 @@ const fibs = require('./problem').fibs;
 const solve = require('./problem').solve;
 
 describe('Problem 2: Even Fibonacci numbers', () => {
-  describe("fibs", function () {
-    it("should be a function", function () {
+  describe('fibs', () => {
+    it('should be a function', () => {
       expect(fibs).to.be.a('function');
     });
-    it("should return all Fibonacci numbers under n", function () {
+    it('should return all Fibonacci numbers under n', () => {
       expect(fibs(10)).to.be.an('array');
       expect(fibs(89)).to.deep.equal([1, 2, 3, 5, 8, 13, 21, 34, 55]);
       expect(fibs(89 + 1)).to.deep.equal([1, 2, 3, 5, 8, 13, 21, 34, 55, 89]);
     });
   });
-  describe("solve", function () {
-    it("should be a function", function () {
+  describe('solve', () => {
+    it('should be a function', () => {
       expect(solve).to.be.a('function');
     });
-    it("should return the sum of only the even first n Fibonacci numbers", () => {
+    it('should return the sum of only the even-valued Fibonacci numbers under n', () => {
       expect(solve(10)).to.equal(10);
     });
   });
