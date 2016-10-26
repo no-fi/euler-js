5bae5467fe709698a6220c83832af9008f0b9501
Christopher Kiel <no-fi@users.noreply.github.com>
2016-10-25 22:10:26 -0500

    Lints 14/test.spec.js

--- 14/test.spec.js
+++ 14/test.spec.js
@@ -1,15 +1,15 @@
 const expect = require('chai').expect;
 const collatz = require('./problem').collatz;
 
-describe("Problem 14: Longest Collatz Sequence", function () {
-  describe("collatz", function () {
-    it("should be a function", function () {
+describe('Problem 14: Longest Collatz Sequence', () => {
+  describe('collatz', () => {
+    it('should be a function', () => {
       expect(collatz).to.be.a('function');
     });
-    it("should return 1 when n is 1", function () {
+    it('should return 1 when n is 1', () => {
       expect(collatz(1)).to.equal(1);
     });
-    it("should return the number of terms in sequence starting with n until 1 is reached", function () {
+    it('should return the number of terms in sequence starting with n until 1 is reached', () => {
         expect(collatz(13)).to.equal(10);
     });
   });
