4fb189c093e51b7736fbdf5a0dd68e5a3511bb0c
Christopher Kiel <no-fi@users.noreply.github.com>
2016-11-01 21:21:17 -0500

    refactors tests for problem 31

--- 31/test.spec.js
+++ 31/test.spec.js
@@ -1,11 +1,11 @@
 const expect = require('chai').expect;
 const count = require('./problem');
 
-describe("Problem 31: Coin Sums", function () {
-  it("should return a count of 1 way to make change for 1p", function () {
+describe('Problem 31: Coin Sums', () => {
+  it('should return a count of 1 way to make change for 1p', () => {
     expect(count(1)).to.equal(1);
   });
-  it("should return a count of 3 ways to make change for 4p", function () {
+  it('should return a count of 3 ways to make change for 4p', () => {
     expect(count(4)).to.equal(3);
   });
 });
