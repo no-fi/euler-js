cd39e9dd5b3c8b0b09dd45a46a0c22912a2a7dd7
Christopher Kiel <no-fi@users.noreply.github.com>
2016-11-02 21:06:05 -0500

    Lints test for problem 40

--- 40/test.spec.js
+++ 40/test.spec.js
@@ -1,15 +1,15 @@
 const expect = require('chai').expect;
 const digits = require('./problem').digits;
 
-describe("Problem 40: Champernowne's constant", function () {
-  describe("digits", function () {
-      it("should return an array of digits", function () {
+describe('Problem 40: Champernowne\'s constant', () => {
+  describe('digits', () => {
+      it('should return an array of digits', () => {
         expect(digits()).to.deep.equal([]);
       });
-      it("should return 1 as the 12th digit", function () {
+      it('should return 1 as the 12th digit', () => {
         expect(digits([12])).to.deep.equal([1]);
       });
-      it("should return an array of the digits at all provided positions", function () {
+      it('should return an array of the digits at all provided positions', () => {
         expect(digits([1])).to.deep.equal([1]);
         expect(digits([1,10])).to.deep.equal([1, 1]);
       });
